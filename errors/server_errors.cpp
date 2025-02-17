c2e18230053bf21d127478df421a47228c3b4728
Berhan Berk Akgun <bakgun@k2m12s01.42kocaeli.com.tr>
2025-02-17 20:25:28 +0300

    error_fixing

--- errors/server_errors.cpp
+++ errors/server_errors.cpp
@@ -1,57 +1,60 @@
 #include "../includes/server.hpp"
 
-void Server::noSuchChannel(int fd, std::string channelName)
+void noSuchChannel(Client *client, std::string channelName)
 {
-	Client *client = this->getClient(fd);
 	if (!client)
 		return;
 	std::string errMsg = ":ircserv 403 " + client->getFullIdenifer() + " " + channelName + " :No such channel";
-	return sendMessage(fd, errMsg);
+	return sendMessage(client->getFd(), errMsg);
 }
 
-void Server::noSuchNick(int fd, std::string nickName)
+#include <iostream>
+void noSuchNick(Client *client, std::string nickName, std::string channelName)
 {
-	Client *client = this->getClient(fd);
+	std::string errMsg;
 	if (!client)
 		return;
-	std::string errMsg = ":ircserv 401 " + client->getFullIdenifer() + " " + nickName + " :No such nick";
-	return sendMessage(fd, errMsg);
+	if (channelName.empty())
+	{
+		std::string errMsg = ":ircserv 401 " + client->getFullIdenifer() + " " + nickName + " :No such nick";
+		return sendMessage(client->getFd(), errMsg);
+	}
+	errMsg = ":ircserv 401 " + client->getFullIdenifer() + " " + channelName + " " + nickName + " :No such nick";
+	sendMessage(client->getFd(), errMsg);
+	errMsg = ":ircserv 401 " + client->getFullIdenifer() + " " + nickName + " :No such nick";
+	return sendMessage(client->getFd(), errMsg);
 }
 
-void Server::permissionDenied(int fd, Channels channel)
+void permissionDenied(Client *client, Channels channel)
 {
-	Client *client = this->getClient(fd);
 	if (!client)
 		return;
 	std::string errMsg = ":ircserv 482 " + client->getFullIdenifer() + " " + channel.getChannelName() + " :You are not channel operator!";
-	return sendMessage(fd, errMsg);
+	return sendMessage(client->getFd(), errMsg);
 }
 
-void Server::notInThatChannel(int fd, Channels channel)
+void notInThatChannel(Client *client, Channels channel)
 {
-	Client *client = this->getClient(fd);
 	if (!client)
 		return;
 	std::string errMsg = ":ircserv 442 " + client->getFullIdenifer() + " " + channel.getChannelName() + " :You're not on that channel";
-	return sendMessage(fd, errMsg);
+	return sendMessage(client->getFd(), errMsg);
 }
 
-void Server::invalidChannelName(int fd, std::string channelName)
+void invalidChannelName(Client *client, std::string channelName)
 {
-	Client *client = this->getClient(fd);
 	if (!client)
 		return;
 	std::string errMsg = ":ircserv 476 " + client->getFullIdenifer() + " " + channelName + " :Invalid channel name, must start with #";
-	return sendMessage(fd, errMsg);
+	return sendMessage(client->getFd(), errMsg);
 }
 
-void Server::notEnoughParameters(int fd, std::string command)
+void notEnoughParameters(Client *client, std::string command)
 {
-	Client *client = this->getClient(fd);
 	if (!client)
 		return;
 	std::string errMsg = ":ircserv 461 " + client->getFullIdenifer() + " " + command + " :Not enough parameters";
-	return sendMessage(fd, errMsg);
+	return sendMessage(client->getFd(), errMsg);
 }
 
 void Channels::unknownModeFlag(int fd, char flag)
@@ -59,6 +62,6 @@ void Channels::unknownModeFlag(int fd, char flag)
 	Client *client = this->getClient(fd);
 	if (!client)
 		return;
-	std::string errMsg = ":ircserv 501 " + client->getFullIdenifer() + " " + this->getChannelName() + " " + std::to_string(flag) + " :is unknown mode flag";
+	std::string errMsg = ":ircserv 501 " + client->getFullIdenifer() + " " + this->getChannelName() + " " + flag + " :is unknown mode flag";
 	return sendMessage(fd, errMsg);
 }
